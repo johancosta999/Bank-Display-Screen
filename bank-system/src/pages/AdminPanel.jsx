b4d2a0bad5bc093c02f05498d902bc73c88bf5b7
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-06-27 11:11:09 +0530

    first commit

---
+++ bank-system/src/pages/AdminPanel.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+function AdminPanel() {
+    const handleSubmit = () => {
+        return
+    }
+
+  return (
+    <div>
+      <button>Task Finished</button>
+    </div>
+  )
+}
+
+export default AdminPanel
